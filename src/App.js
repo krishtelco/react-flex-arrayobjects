2736059e5804bd43d2888bc99667a78fb3a3fb02
krishtelco <krishtelco@gmail.com>
2022-05-20 13:34:47 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -14,12 +14,22 @@ export default function App() {
 
     setFruits([...oldfruits]);
   };
+  const ondelete2 = (item) => {
+    
+    let index= fruits.indexOf(item);
+    setFruits([...fruits.slice(0,index), ...fruits.slice(index+1,fruits.length)]);
+
+    
+  };
   return (
     <div className="flexbox">
       <div className="flexitem">
         <h1>Hello </h1>
         <button onClick={onsubmit}>add</button>{' '}
         <button onClick={() => ondelete('Apple')}>del</button>
+        {' '}
+        <button onClick={() => ondelete2('Orange')}>del2</button>
+        <br/>
         {fruits.map((fruit) => (
           <li>{fruit}</li>
         ))}
