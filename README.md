30e17ca7a2dc09392f583547f16b75cb15af9f21
krishtelco <krishtelco@gmail.com>
2022-05-20 10:49:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-h3ndr2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-h3ndr2)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="flexbox">
+      <div className="flexitem">
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      </div>
+    </div>
+  );
+}
