3dd73283a73aa896734530817ec536ad3d9c7228
krishtelco <krishtelco@gmail.com>
2022-05-20 12:47:26 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [fruits, setFruits] = useState(['Banana', 'Orange', 'Apple', 'Mango']);
+
+  const onsubmit = () => {
+    setFruits([...fruits, 'newfruit']);
+  };
+  const ondelete = (item) => {
+    let oldfruits = [...fruits];
+
+    oldfruits.splice(oldfruits.indexOf(item), 1);
+
+    setFruits([...oldfruits]);
+  };
   return (
     <div className="flexbox">
       <div className="flexitem">
-      <h1>Hello </h1>
-      <h1>Hello </h1>
-      
+        <h1>Hello </h1>
+        <button onClick={onsubmit}>add</button>{' '}
+        <button onClick={() => ondelete('Apple')}>del</button>
+        {fruits.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
       </div>
     </div>
   );
