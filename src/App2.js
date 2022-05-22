8c79475def64fabea84f2e3c55adaf11a0f78489
krishtelco <krishtelco@gmail.com>
2022-05-22 06:26:24 +0530

    1 file modified

--- src/App2.js
+++ src/App2.js
@@ -38,7 +38,7 @@ export default function App2() {
   };
   const modify = (sname, mark) => {
     let index = students.findIndex((stuobj) => stuobj.name == sname);
-    let newstudent = { ...students[index], ['mark1']: mark };
+    let newstudent = { ...students[index], mark1: mark };
     
     let oldstudents = [...students];
     oldstudents[index] = newstudent;
